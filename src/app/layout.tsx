37f28eea8494bbed58c9fcdb3549ff01fb9c31da
gyanEkka <gyan.ekka@mathongo.com>
2025-03-16 19:42:19 +0530

    style (font) : updated to Work_Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { Work_Sans } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import { Providers } from "@/components/providers";
 
@@ -10,13 +11,18 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const work_sans = Work_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className={work_sans.className} suppressHydrationWarning>
       <body className="antialiased">
         <Providers>
           {children}
