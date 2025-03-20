8dee268b41e551c26735706b6f033d3e850d530d
anupam <anupamminz300am@gmail.com>
2025-03-20 15:27:10 +0530

    trying layouts

--- src/app/dashboard/form/create/page.tsx
+++ src/app/dashboard/form/create/page.tsx
@@ -1,6 +1,26 @@
 export default function FormCreationPage() {
   return (
     <div>
+      <form className="bg-white">
+        <select>
+          <option>address</option>
+          <option>identity</option>
+        </select>
+      </form>
+
+      display it in this type?
+
+      <form className="bg-white dark:bg-black flex flex-col gap-2 p-4 rounded" action="" method="POST">
+        <label className="flex flex-col gap-2"><span className="">full name</span>
+          <input type="text" name="fullname" className="border border-black bg-white dark:bg-black rounded" />
+        </label>
+        <label className="flex flex-col gap-2"><span className="">email</span>
+          <input type="email" name="email" className="border border-black bg-white dark:bg-black rounded" />
+        </label>
+        <label className="flex flex-col gap-2"><span className="">phone number</span>
+          <input type="text" name="phoneno" className="border border-black bg-white dark:bg-black rounded" />
+        </label>
+      </form>
       <div>form creation page</div>
     </div>
   );
