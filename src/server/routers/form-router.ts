82f7d0c32f77d7ea8fed68f455d3aec6ee652fab
anupam <anupamminz300am@gmail.com>
2025-03-20 16:06:25 +0530

    add: create new form

--- src/server/routers/form-router.ts
+++ src/server/routers/form-router.ts
@@ -5,12 +5,22 @@ import { j, authenticatedProcedure } from "../jstack";
 import { eq } from "drizzle-orm";
 
 export const formRouter = j.router({
-  create: authenticatedProcedure.get(async ({c, ctx}) => {
+  create: authenticatedProcedure.get(async ({ c, ctx }) => {
     const workspaceId = parseInt(c.req.query("workspace_id"));
+    const { db, session } = ctx;
     if (isNaN(workspaceId)) {
       throw new Error("failed to parse workspace id");
     }
     console.log(workspaceId);
+    const createdForm = await db
+      .insert(tables.form)
+      .values({
+        userId: session.user.id,
+        workspaceId: workspaceId,
+      })
+      .returning();
+    console.log(createdForm);
+
     return c.redirect("/dashboard/form/create", 302);
   }),
 });
