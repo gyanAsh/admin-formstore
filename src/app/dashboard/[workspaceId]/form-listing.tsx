ddf45f759c6d19565fc05c4e5f8f15f62107f65b
gyanEkka <gyan.ekka@mathongo.com>
2025-03-22 17:24:18 +0530

    chore (table): dummy form table added

--- src/app/dashboard/[workspaceId]/form-listing.tsx
+++ src/app/dashboard/[workspaceId]/form-listing.tsx
@@ -19,11 +19,22 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 import { Skeleton } from "@/components/ui/skeleton";
 import { client } from "@/lib/client";
 import { useQuery } from "@tanstack/react-query";
 import { Ellipsis } from "lucide-react";
+import React from "react";
+import { useVirtualizer } from "@tanstack/react-virtual";
 
 export const FormListing = () => {
   const {
@@ -43,8 +54,17 @@ export const FormListing = () => {
     loading_current_workspace,
     fetched_current_workspace,
   });
+  const parentRef = React.useRef(null);
+
+  // The virtualizer
+  const rowVirtualizer = useVirtualizer({
+    count: 25,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 35,
+  });
+  console.log({ rowVirtualizer });
   return (
-    <Card className="h-full">
+    <Card className="h-full gap-3">
       {loading_current_workspace ? (
         <section className="flex flex-col gap-3 m-4">
           <Skeleton className="w-[120px] h-[40px]" />
@@ -68,7 +88,12 @@ export const FormListing = () => {
               </CardHeader>
               <Popover>
                 <PopoverTrigger asChild>
-                  <Button variant="outline" size={"icon"} effect="click">
+                  <Button
+                    variant="outline"
+                    size={"icon"}
+                    effect="click"
+                    className="size-6.5"
+                  >
                     <Ellipsis strokeWidth={3} />
                   </Button>
                 </PopoverTrigger>
@@ -84,7 +109,7 @@ export const FormListing = () => {
             </div>
             <div className="flex gap-2 mx-6">
               <Select>
-                <SelectTrigger className="w-[180px]">
+                <SelectTrigger className="w-[95px]">
                   <SelectValue placeholder="Filter" />
                 </SelectTrigger>
                 <SelectContent>
@@ -94,7 +119,7 @@ export const FormListing = () => {
                 </SelectContent>
               </Select>
               <Select>
-                <SelectTrigger className="w-[180px]">
+                <SelectTrigger className="w-[95px]">
                   <SelectValue placeholder="Sort" />
                 </SelectTrigger>
                 <SelectContent>
@@ -106,17 +131,53 @@ export const FormListing = () => {
             </div>
           </section>
           <CardContent className="flex flex-col gap-2">
-            asdfadsf
-            <Button>
-              asdfasdfffffffff
-              <br />
-              asdffffffff
-            </Button>
-            <Button>
-              asdfasdfffffffff
-              <br />
-              asdffffffff
-            </Button>
+            {/* The scrollable element for your list */}
+            <div className="grid grid-cols-5 gap-4 text-start">
+              <div>ID</div>
+              <div>Title</div>
+              <div>Status</div>
+              <div>Response</div>
+              <div>Updated</div>
+            </div>
+            <div
+              ref={parentRef}
+              style={{
+                height: `calc(100dvh - 180px)`,
+                overflow: "auto", // Make it scroll!
+              }}
+            >
+              {/* The large inner element to hold all of the items */}
+              <div
+                style={{
+                  height: `${rowVirtualizer.getTotalSize()}px`,
+                  width: "100%",
+                  position: "relative",
+                }}
+              >
+                {/* Only the visible items in the virtualizer, manually positioned to be in view */}
+                {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+                  <Button
+                    key={virtualItem.key}
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: `${virtualItem.size}px`,
+                      transform: `translateY(${virtualItem.start}px)`,
+                    }}
+                    variant={"violet_secondary"}
+                    className="grid grid-cols-5 gap-4 text-start border-b-2 active:scale-[0.998] active:translate-y-[3px]"
+                  >
+                    <div>ID{virtualItem.index}</div>
+                    <div>Form {virtualItem.index}</div>
+                    <div>{virtualItem.index % 3 ? "Draft" : "Published"}</div>
+                    <div>{virtualItem.index * 7}</div>
+                    <div>08 Mar 2025</div>
+                  </Button>
+                ))}
+              </div>
+            </div>
           </CardContent>
         </>
       ) : (
