4e09a3cde572df3f3894e26f616425d84c4fb6fc
gyanEkka <gyan.ekka@mathongo.com>
2025-03-23 21:35:29 +0530

    chore (ui): basis ui layout added

--- src/app/dashboard/[workspaceId]/[formId]/page.tsx
+++ src/app/dashboard/[workspaceId]/[formId]/page.tsx
@@ -1,27 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+
 export default function FormCreationPage() {
   return (
-    <div>
-      <form className="bg-white">
-        <select>
-          <option>address</option>
-          <option>identity</option>
-        </select>
-      </form>
+    // <div>
+    //   <form className="bg-white">
+    //     <select>
+    //       <option>address</option>
+    //       <option>identity</option>
+    //     </select>
+    //   </form>
 
-      display it in this type?
+    //   display it in this type?
 
-      <form className="bg-white dark:bg-black flex flex-col gap-2 p-4 rounded" action="" method="POST">
-        <label className="flex flex-col gap-2"><span className="">full name</span>
-          <input type="text" name="fullname" className="border border-black bg-white dark:bg-black rounded" />
-        </label>
-        <label className="flex flex-col gap-2"><span className="">email</span>
-          <input type="email" name="email" className="border border-black bg-white dark:bg-black rounded" />
-        </label>
-        <label className="flex flex-col gap-2"><span className="">phone number</span>
-          <input type="text" name="phoneno" className="border border-black bg-white dark:bg-black rounded" />
-        </label>
-      </form>
-      <div>form creation page</div>
-    </div>
+    //   <form className="bg-white dark:bg-black flex flex-col gap-2 p-4 rounded" action="" method="POST">
+    //     <label className="flex flex-col gap-2"><span className="">full name</span>
+    //       <input type="text" name="fullname" className="border border-black bg-white dark:bg-black rounded" />
+    //     </label>
+    //     <label className="flex flex-col gap-2"><span className="">email</span>
+    //       <input type="email" name="email" className="border border-black bg-white dark:bg-black rounded" />
+    //     </label>
+    //     <label className="flex flex-col gap-2"><span className="">phone number</span>
+    //       <input type="text" name="phoneno" className="border border-black bg-white dark:bg-black rounded" />
+    //     </label>
+    //   </form>
+    //   <div>form creation page</div>
+    // </div>
+    <Card className="w-full flex flex-col justify-between">
+      <CardHeader>edit option goes herer</CardHeader>
+      <CardContent>here should be the form's page preview</CardContent>
+      <CardFooter>page slider goes here</CardFooter>
+    </Card>
   );
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,35 +1,19 @@
-import type { Metadata } from "next";
-
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
 import { AppSidebar } from "@/components/app-sidebar";
 import { TopNavbar } from "./top-navbar";
 
-export const metadata: Metadata = {
-  title: "JStack App",
-  description: "Created using JStack",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className="antialiased">
-        <SidebarProvider className="bg-violet-500 dark:bg-violet-600/80">
-          <AppSidebar />
-          <div className="flex flex-col w-full">
-            <TopNavbar />
-            <div className="w-full h-full flex p-4 relative isolate">
-              {children}
-            </div>
-          </div>
-          <Toaster />
-        </SidebarProvider>
-      </body>
-    </html>
+    <SidebarProvider className="bg-violet-500 dark:bg-violet-600/80">
+      <AppSidebar />
+      <div className="flex flex-col w-full">
+        <TopNavbar />
+        <div className="w-full h-full flex p-4 relative isolate">
+          {children}
+        </div>
+      </div>
+      <Toaster />
+    </SidebarProvider>
   );
 }
