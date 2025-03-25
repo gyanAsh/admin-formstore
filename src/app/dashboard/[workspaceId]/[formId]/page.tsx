3b91af5c4cc86905a6378f2b3f410d04df0c73bf
gyanEkka <gyan.ekka@mathongo.com>
2025-03-25 05:44:53 +0530

    ui-fix (form): form overflow on dynamic screens

--- src/app/dashboard/[workspaceId]/[formId]/page.tsx
+++ src/app/dashboard/[workspaceId]/[formId]/page.tsx
@@ -1,92 +1,65 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { useSidebar } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
-import { MonitorSmartphone } from "lucide-react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+import { Minus, MonitorSmartphone, Plus, RotateCw } from "lucide-react";
+
 import React from "react";
 
 export default function FormCreationPage() {
   const { isMobile } = useSidebar();
   const [previewView, setPreviewView] = React.useState("laptop");
+  const [zoom, setZoom] = React.useState(1);
+
   return (
-    // <div>
-    //   <form className="bg-white">
-    //     <select>
-    //       <option>address</option>
-    //       <option>identity</option>
-    //     </select>
-    //   </form>
-
-    //   display it in this type?
-
-    //   <form className="bg-white dark:bg-black flex flex-col gap-2 p-4 rounded" action="" method="POST">
-    //     <label className="flex flex-col gap-2"><span className="">full name</span>
-    //       <input type="text" name="fullname" className="border border-black bg-white dark:bg-black rounded" />
-    //     </label>
-    //     <label className="flex flex-col gap-2"><span className="">email</span>
-    //       <input type="email" name="email" className="border border-black bg-white dark:bg-black rounded" />
-    //     </label>
-    //     <label className="flex flex-col gap-2"><span className="">phone number</span>
-    //       <input type="text" name="phoneno" className="border border-black bg-white dark:bg-black rounded" />
-    //     </label>
-    //   </form>
-    //   <div>form creation page</div>
-    // </div>
     <Card
-      className={"justify-between bg-sidebar-accent w-full h-full relative"}
+      className={
+        "justify-between bg-sidebar-accent w-full max-h-[calc(100dvh_-_86px)] relative gap-3"
+      }
     >
-      <CardHeader className="  ">edit option goes herer</CardHeader>
-      <CardContent className="flex h-full justify-between items-center gap-4">
-        <Card className="h-full w-fit">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  size={"icon"}
-                  variant={"secondary"}
-                  effect={"click"}
-                  onClick={() => {
-                    setPreviewView((e) =>
-                      e === "laptop" ? "mobile" : "laptop"
-                    );
-                  }}
-                >
-                  <MonitorSmartphone />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Toogle Preview Screen</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </Card>
-        <div
-          className=" w-full h-full
-           overflow-auto flex items-center justify-center"
+      <CardHeader className="flex flex-row items-center sticky top-0 left-0 z-10">
+        edit option goes herer
+        <Button
+          onClick={() =>
+            setPreviewView((e) => (e === "laptop" ? "mobile" : "laptop"))
+          }
         >
-          <div
+          <MonitorSmartphone />
+        </Button>
+        <Button
+          onClick={() =>
+            setZoom((e) =>
+              Math.min(e * 1.1, isMobile || previewView === "mobile" ? 5 : 1.5)
+            )
+          }
+        >
+          <Plus />
+        </Button>
+        <Button onClick={() => setZoom((e) => Math.max(e / 1.1, 0.8))}>
+          <Minus />
+        </Button>
+        <Button onClick={() => setZoom(1)}>
+          <RotateCw />
+        </Button>
+      </CardHeader>
+      <div className=" overflow-auto">
+        <div className="flex h-full justify-center items-start">
+          <ScrollArea
             className={cn(
-              "drop-shadow-lg shadow-lg border-2 border-slate-600 bg-rose-600 h-[calc(100dvh-350px)]  w-[calc((100dvh-350px)*16/9)] box-border overflow-auto",
+              "drop-shadow-lg shadow-lg border-2 border-slate-600 bg-rose-600 h-[calc(100dvh-350px)]  w-[calc((100dvh-350px)*16/9)]",
+              "box-border overflow-auto",
               {
                 "h-[calc(100dvh-350px)]  w-[calc((100dvh-350px)*9/16)]":
                   isMobile || previewView === "mobile",
               }
             )}
+            style={{
+              zoom: zoom,
+            }}
           >
-            asdfdsfdsfsdfdfssss <br />
+            fasdfdsfdsfsdfdfssss <br />
             asdfdsfdsfsdfdfssss <br />
             asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss
             <br /> asdfdsfdsfsdfdfssss
@@ -94,6 +67,48 @@ export default function FormCreationPage() {
             asdfdsfdsfsdfdfssss <br />
             asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss
             <br /> asdfdsfdsfsdfdfssss
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
             <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
             ddddddddddddddddddd
             <br /> ddddddddddddlllllllllll lllllllllllll lllllllllllllllllll
@@ -104,33 +119,12 @@ export default function FormCreationPage() {
             <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
             ddddddddddddddddddd
             <br /> ddddddddddddlllllllllll lllllllllllll lllllllllllllllllll
-          </div>
+            <ScrollBar />
+          </ScrollArea>
         </div>
-        <Card className="h-full w-fit">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  size={"icon"}
-                  variant={"secondary"}
-                  effect={"click"}
-                  onClick={() => {
-                    setPreviewView((e) =>
-                      e === "laptop" ? "mobile" : "laptop"
-                    );
-                  }}
-                >
-                  <MonitorSmartphone />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Toogle Preview Screen</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </Card>
-      </CardContent>
-      <CardFooter className=" sticky bottom-0">
+        {/* <ScrollBar orientation="vertical" /> */}
+      </div>
+      <CardFooter className="sticky bottom-0 left-0">
         <ScrollArea className="w-full whitespace-nowrap rounded-md border">
           <div className="flex space-x-4 p-4 overflow-hidden">
             {Array.from({ length: 25 }).map((_, index) => (
