c4ee83ed1bf83d78024b3979adc32669fca974f4
gyanEkka <gyan.ekka@mathongo.com>
2025-03-26 06:32:50 +0530

    feat (react-zoom): page zoom feature added

--- src/app/dashboard/[workspaceId]/[formId]/page.tsx
+++ src/app/dashboard/[workspaceId]/[formId]/page.tsx
@@ -2,128 +2,93 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { useSidebar } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { Minus, MonitorSmartphone, Plus, RotateCw } from "lucide-react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 import React from "react";
 
 export default function FormCreationPage() {
-  const { isMobile } = useSidebar();
   const [previewView, setPreviewView] = React.useState("laptop");
-  const [zoom, setZoom] = React.useState(1);
-
   return (
     <Card
       className={
         "justify-between bg-sidebar-accent w-full max-h-[calc(100dvh_-_86px)] relative gap-3"
       }
     >
-      <CardHeader className="flex flex-row items-center sticky top-0 left-0 z-10">
-        edit option goes herer
-        <Button
-          onClick={() =>
-            setPreviewView((e) => (e === "laptop" ? "mobile" : "laptop"))
-          }
-        >
-          <MonitorSmartphone />
-        </Button>
-        <Button
-          onClick={() =>
-            setZoom((e) =>
-              Math.min(e * 1.1, isMobile || previewView === "mobile" ? 5 : 1.5)
-            )
-          }
-        >
-          <Plus />
-        </Button>
-        <Button onClick={() => setZoom((e) => Math.max(e / 1.1, 0.8))}>
-          <Minus />
-        </Button>
-        <Button onClick={() => setZoom(1)}>
-          <RotateCw />
-        </Button>
-      </CardHeader>
-      <div className=" overflow-auto">
-        <div className="flex h-full justify-center items-start">
-          <ScrollArea
-            className={cn(
-              "drop-shadow-lg shadow-lg border-2 border-slate-600 bg-rose-600 h-[calc(100dvh-350px)]  w-[calc((100dvh-350px)*16/9)]",
-              "box-border overflow-auto",
-              {
-                "h-[calc(100dvh-350px)]  w-[calc((100dvh-350px)*9/16)]":
-                  isMobile || previewView === "mobile",
-              }
-            )}
-            style={{
-              zoom: zoom,
-            }}
-          >
-            fasdfdsfdsfsdfdfssss <br />
-            asdfdsfdsfsdfdfssss <br />
-            asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss
-            <br /> asdfdsfdsfsdfdfssss
-            <br /> asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss <br />
-            asdfdsfdsfsdfdfssss <br />
-            asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss
-            <br /> asdfdsfdsfsdfdfssss
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            ddddddddddddddddddd
-            <br /> ddddddddddddlllllllllll lllllllllllll lllllllllllllllllll
-            <br /> asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss <br />
-            asdfdsfdsfsdfdfssss <br />
-            asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss
-            <br /> asdfdsfdsfsdfdfssss
-            <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
-            ddddddddddddddddddd
-            <br /> ddddddddddddlllllllllll lllllllllllll lllllllllllllllllll
-            <ScrollBar />
-          </ScrollArea>
-        </div>
-        {/* <ScrollBar orientation="vertical" /> */}
-      </div>
+      <TransformWrapper
+        initialScale={1}
+        centerOnInit
+        centerZoomedOut
+        initialPositionX={100}
+        initialPositionY={50}
+        minScale={0.7}
+      >
+        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+          <React.Fragment>
+            <CardHeader className="flex flex-row items-center sticky top-0 left-0 z-10">
+              edit option goes herer
+              <Button
+                onClick={() =>
+                  setPreviewView((e) => (e === "laptop" ? "mobile" : "laptop"))
+                }
+              >
+                <MonitorSmartphone />
+              </Button>
+              <Button onClick={() => zoomIn()}>
+                <Plus />
+              </Button>
+              <Button onClick={() => zoomOut()}>
+                <Minus />
+              </Button>
+              <Button onClick={() => resetTransform()}>
+                <RotateCw />
+              </Button>
+            </CardHeader>
+            <TransformComponent wrapperClass="flex !h-full !w-full">
+              <div
+                className={cn(
+                  "drop-shadow-lg shadow-lg border border-slate-600   h-[calc(100dvh-350px)]  w-[calc((100dvh-350px)*16/9)]",
+                  "box-border overflow-hidden",
+                  {
+                    "h-[calc(100dvh-350px)]  w-[calc((100dvh-350px)*9/16)]":
+                      previewView === "mobile",
+                  }
+                )}
+              >
+                fasdfdsfdsfsdfdfssss <br />
+                asdfdsfdsfsdfdfssss <br />
+                asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss
+                <br /> asdfdsfdsfsdfdfssss
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                <div>asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd</div>
+                asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+                asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+                asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+                <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+                ddddddddddddddddddd
+                <br /> ddddddddddddlllllllllll lllllllllllll lllllllllllllllllll
+                <br /> asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss <br />
+                asdfdsfdsfsdfdfssss <br />
+                asdfdsfdsfsdfdfssss asdfdsfdsfsdfdfssss
+                <br /> asdfdsfdsfsdfdfssss
+                <br /> asdfdsfdsfsdfdfssss dsssdsdsdddddddddddddd <br />
+                ddddddddddddddddddd
+                <br /> ddddddddddddlllllllllll lllllllllllll lllllllllllllllllll
+              </div>
+            </TransformComponent>
+          </React.Fragment>
+        )}
+      </TransformWrapper>
+
       <CardFooter className="sticky bottom-0 left-0">
         <ScrollArea className="w-full whitespace-nowrap rounded-md border">
           <div className="flex space-x-4 p-4 overflow-hidden">
