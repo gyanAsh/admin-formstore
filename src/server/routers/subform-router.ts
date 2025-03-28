0afc0f5da3a18fe78b2dad561dc56649810054fe
anupam <anupamminz300am@gmail.com>
2025-03-28 16:56:37 +0530

    handle error in case of extra inserts

--- src/server/routers/subform-router.ts
+++ src/server/routers/subform-router.ts
@@ -2,7 +2,7 @@ import * as tables from "@/server/db/schema";
 import { desc } from "drizzle-orm";
 import { z } from "zod";
 import { j, authenticatedProcedure } from "../jstack";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
 export const subformRouter = j.router({
   create: authenticatedProcedure
@@ -31,14 +31,37 @@ export const subformRouter = j.router({
         return;
       }
 
-      const subformResults = await db.insert(tables.form_subform).values({
-        formId: input.formId,
-        sequenceNumber: input.sequenceNumber,
-        subformType: input.elementType,
-      }).returning({subformId: tables.form_subform.id});
-
-      return c.superjson({
-        subformId: subformResults[0].subformId,
-      });
+      try {
+        const subformResults = await db
+          .insert(tables.form_subform)
+          .values({
+            formId: input.formId,
+            sequenceNumber: input.sequenceNumber,
+            subformType: input.elementType,
+          })
+          .returning({ subformId: tables.form_subform.id });
+        c.status(201);
+        return c.superjson({
+          subformId: subformResults[0].subformId,
+        });
+      } catch (err) {
+        console.error(err);
+        const subformResults = await db
+          .select()
+          .from(tables.form_subform)
+          .where(
+            and(
+              eq(tables.form_subform.formId, input.formId),
+              eq(tables.form_subform.sequenceNumber, input.sequenceNumber),
+            ),
+          );
+        c.status(409);
+        console.log(subformResults[0]);
+        return c.superjson({
+          subformId: subformResults[0].id,
+          elementType: subformResults[0].subformType,
+          sequenceNumber: subformResults[0].sequenceNumber,
+        });
+      }
     }),
 });
