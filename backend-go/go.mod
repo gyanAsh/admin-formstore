5110881f00196a5f97d48d80b522b34ef158f3b4
anupam <anupamminz300am@gmail.com>
2025-04-03 15:28:34 +0530

    add token methods

--- backend-go/go.mod
+++ backend-go/go.mod
@@ -4,6 +4,7 @@ go 1.24.1
 
 require (
 	github.com/antonlindstrom/pgstore v0.0.0-20220421113606-e3a6e3fed12a // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
---
+++ backend-go/src/services/tokens.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func generateAuthToken(userID int64, signedSecret []byte) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+	})
+	tokenString, err := token.SignedString(signedSecret)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func parseAuthToken(tokenString string, signedSecret []byte) (int64, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return signedSecret, nil
+	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+	if err != nil {
+		return 0, err
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		userID_f, ok := claims["user_id"].(float64)
+		if !ok {
+			fmt.Errorf("failed to parse claims with Error: failed to parse user_id to float64")
+		}
+		return int64(userID_f), nil
+	}
+	return 0, fmt.Errorf("failed to parse claims")
+}
