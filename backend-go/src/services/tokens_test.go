0df7f43316c05411df7e26c242b6849778eccf7f
anupam <anupamminz300am@gmail.com>
2025-04-03 15:44:10 +0530

    test tokens

---
+++ backend-go/src/services/tokens_test.go
@@ -0,0 +1,19 @@
+package services
+
+import "testing"
+
+func TestCreateAndParseToken(t *testing.T) {
+	var userID int64 = 11224431
+	signedSecret := []byte("sample secret")
+	tokenString, err := generateAuthToken(userID, signedSecret)
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedUserID, err := parseAuthToken(tokenString, signedSecret)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if userID != parsedUserID {
+		t.Fatal("orignal value and parse values don't match", userID, parsedUserID)
+	}
+}
