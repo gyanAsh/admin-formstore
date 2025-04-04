4bcd696aa6bd766caf3cc0d2db81dc985b9b71b2
anupam <anupamminz300am@gmail.com>
2025-04-04 11:46:57 +0530

    fix: json encoding bug for workspace

--- backend-go/src/services/workspace.go
+++ backend-go/src/services/workspace.go
@@ -17,7 +17,7 @@ type Workspace struct {
 	Name      string    `json:"name"`
 	UserID    int64     `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (s *Service) workspaceCreateHandler(w http.ResponseWriter, r *http.Request) {
