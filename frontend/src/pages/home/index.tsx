b8be7c507f5f83a05aa4309abc6c4d49aa6f36e4
gyanEkka <gyan.ekka@mathongo.com>
2025-04-12 11:07:05 +0530

    fix (api): removed testing endpoint

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,52 +1,39 @@
 import { memo } from "react";
 import { cn } from "@/lib/utils";
 import { NavLink } from "react-router";
-import endpoint_home from "./endpoint";
-import { $counter } from "@/store/count";
-import { useQuery } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 
 export default memo(function Home() {
-  const apidata = useQuery({
-    queryKey: [endpoint_home.getJsonData.key],
-    queryFn: () => endpoint_home.getJsonData.func($counter),
-    refetchOnWindowFocus: false,
-  });
-
-  console.log({ data: "isError", apidata });
-
   return (
-    <>
-      <main className="flex min-h-screen w-full flex-col items-center justify-center relative isolate">
-        <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16">
-          <h1
-            className={cn(
-              "inline-flex tracking-tight flex-col gap-1 transition text-center",
-              "font-display text-4xl sm:text-5xl md:text-6xl font-semibold leading-none lg:text-[4rem]",
-              "bg-gradient-to-r from-20% bg-clip-text text-transparent",
-              "from-blue-500 to-purple-500"
-            )}
-          >
-            <span>Home</span>
-          </h1>
+    <main className="flex min-h-screen w-full flex-col items-center justify-center relative isolate">
+      <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16">
+        <h1
+          className={cn(
+            "inline-flex tracking-tight flex-col gap-1 transition text-center",
+            "font-display text-4xl sm:text-5xl md:text-6xl font-semibold leading-none lg:text-[4rem]",
+            "bg-gradient-to-r from-20% bg-clip-text text-transparent",
+            "from-blue-500 to-purple-500"
+          )}
+        >
+          <span>Home</span>
+        </h1>
 
-          <p className="text-lg/7 md:text-xl/8 text-pretty sm:text-wrap sm:text-center text-center mb-8">
-            The stack for building seriously fast, lightweight and{" "}
-            <span className="inline sm:block">this is it's home page.</span>
-          </p>
-        </div>
-        <div className="flex gap-2">
-          <Button asChild>
-            <NavLink to={"/about"}>About</NavLink>
-          </Button>
-          <Button asChild>
-            <NavLink to={"/workspace"}>Workspace (Protected)</NavLink>
-          </Button>
-          <Button asChild>
-            <NavLink to={"/login"}>Login</NavLink>
-          </Button>
-        </div>
-      </main>
-    </>
+        <p className="text-lg/7 md:text-xl/8 text-pretty sm:text-wrap sm:text-center text-center mb-8">
+          The stack for building seriously fast, lightweight and{" "}
+          <span className="inline sm:block">this is it's home page.</span>
+        </p>
+      </div>
+      <div className="flex gap-2">
+        <Button asChild>
+          <NavLink to={"/about"}>About</NavLink>
+        </Button>
+        <Button asChild>
+          <NavLink to={"/workspace"}>Workspace (Protected)</NavLink>
+        </Button>
+        <Button asChild>
+          <NavLink to={"/login"}>Login</NavLink>
+        </Button>
+      </div>
+    </main>
   );
 });
