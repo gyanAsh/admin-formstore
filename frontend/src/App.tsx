aa0ca3c1225e35c02bf07b19d70acd30f89501fc
gyanEkka <gyan.ekka@mathongo.com>
2025-04-12 11:04:32 +0530

    fix (react-query): removed extra queryClient init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,9 @@ import { Route, Routes } from "react-router";
 import DashboardLayout from "@/components/layout/dashboard";
 import Workspace from "@/pages/workspace";
 import HeroLayout from "@/components/layout/hero";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Form from "@/pages/form";
 
 export default function App() {
-  const queryClient = new QueryClient();
-
   const NotFound = () => (
     <div className="flex justify-center items-center">
       <h1>404 - Page Not Found</h1>
@@ -23,20 +20,18 @@ export default function App() {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<HeroLayout />} errorElement={<ErrorFound />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+    <Routes>
+      <Route path="/" element={<HeroLayout />} errorElement={<ErrorFound />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+      <Route path="/workspace" element={<DashboardLayout />}>
+        <Route path=":workspaceId">
+          <Route index element={<Workspace />} />
+          <Route path=":fromId" element={<Form />} />
         </Route>
-        <Route path="/workspace" element={<DashboardLayout />}>
-          <Route path=":workspaceId">
-            <Route index element={<Workspace />} />
-            <Route path=":fromId" element={<Form />} />
-          </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </QueryClientProvider>
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
