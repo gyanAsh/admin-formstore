b5db69c2d3334b5ad3c674253c029910e963373c
anupam <anupamminz300am@gmail.com>
2025-04-25 12:11:55 +0530

    update schema to incorporate form elements

--- backend-go/schema.sql
+++ backend-go/schema.sql
@@ -27,3 +27,30 @@ CREATE TABLE IF NOT EXISTS forms (
 	workspace_id INTEGER NOT NULL,
 	FOREIGN KEY (workspace_id) REFERENCES workspaces(ID)
 );
+
+CREATE TYPE form_element_types AS ENUM ('email', 'phone', 'text', 'date', 'drop_down', 'multiple_selection');
+
+-- This table is used to store form_elements in cases of single element type.
+-- In case of multi values a different table (form_el ement_multi_values)
+-- would be used. That table is references by it's corresponding id to the
+-- original table.
+CREATE TABLE IF NOT EXISTS form_elements (
+	ID SERIAL PRIMARY KEY,
+	element_type form_element_types NOT NULL,
+	value VARCHAR,
+	date_value DATE,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	form_id INTEGER NOT NULL,
+	FOREIGN KEY (form_id) REFERENCES forms(ID)
+);
+
+CREATE TABLE IF NOT EXISTS form_element_multi_values (
+	ID SERIAL PRIMARY KEY,
+	value VARCHAR,
+	date_value DATE,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	form_element_id INTEGER NOT NULL,
+	FOREIGN KEY (form_element_id) REFERENCES form_elements(ID)
+);
