7ec62fe7c1064d2e5026b37fb822296b01751c6d
anupam <anupamminz300am@gmail.com>
2025-04-30 11:15:11 +0530

    query form elements

--- backend-go/src/server/forms.go
+++ backend-go/src/server/forms.go
@@ -3,12 +3,11 @@ package server
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
-
-	"github.com/jackc/pgx/v5"
 )
 
 type Form struct {
@@ -17,6 +16,67 @@ type Form struct {
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 	WorkspaceID string    `json:"workspace_id"`
+}
+
+type FormElement struct {
+	ID    int64  `json:"id,omitempty"`
+	Type  string `json:"type,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+// nil values are pointers
+type RowFormData struct {
+	// form
+	FormID        int64
+	FormTitle     string
+	FormCreatedAt time.Time
+	FormUpdatedAt time.Time
+	// workspace
+	WorkspaceID        int64
+	WorkspaceName      string
+	WorkspaceCreatedAt time.Time
+	WorkspaceUpdatedAt time.Time
+	// user
+	UserID int64
+	// form elements
+	FormElementID    *int64
+	FormElementType  *string
+	FormElementValue *string
+}
+
+type FormData struct {
+	Form         Form          `json:"form"`
+	Workspace    Workspace     `json:"workspace"`
+	FormElements []FormElement `json:"form_elements"`
+}
+
+func parseFormData(rows []RowFormData) (FormData, error) {
+	if len(rows) == 0 {
+		return FormData{}, fmt.Errorf("failed to parse rows: no rows found")
+	}
+	var formData FormData
+	formData.Form.ID = rows[0].FormID
+	formData.Form.Title = rows[0].FormTitle
+	formData.Form.CreatedAt = rows[0].FormCreatedAt
+	formData.Form.UpdatedAt = rows[0].FormUpdatedAt
+	formData.Workspace.ID = rows[0].WorkspaceID
+	formData.Workspace.Name = rows[0].WorkspaceName
+	formData.Workspace.CreatedAt = rows[0].WorkspaceCreatedAt
+	formData.Workspace.UpdatedAt = rows[0].WorkspaceUpdatedAt
+	for _, row := range rows {
+		var element FormElement
+		if row.FormElementID != nil {
+			element.ID = *row.FormElementID
+		}
+		if row.FormElementType != nil {
+			element.Type = *row.FormElementType
+		}
+		if row.FormElementValue != nil {
+			element.Value = *row.FormElementValue
+		}
+		formData.FormElements = append(formData.FormElements, element)
+	}
+	return formData, nil
 }
 
 func (s *Service) formsHandler(w http.ResponseWriter, r *http.Request) {
@@ -159,39 +219,78 @@ func (s *Service) formDataHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	row := s.DB.QueryRow(r.Context(), `
+	rows, err := s.DB.Query(r.Context(), `
 		SELECT
-			forms.ID, forms.title, forms.created_at, forms.updated_at,
-			workspaces.ID, workspaces.name, workspaces.user_id,
-			workspaces.created_at, workspaces.updated_at
+			-- form
+			forms.ID,
+			forms.title,
+			forms.created_at,
+			forms.updated_at,
+			-- workspace
+			workspaces.ID,
+			workspaces.name,
+			workspaces.created_at,
+			workspaces.updated_at,
+			-- user
+			workspaces.user_id,
+			-- form elements (null values, due to left outer join)
+			form_elements.ID,
+			form_elements.element_type,
+			form_elements.value
 		FROM
 			forms
 		INNER JOIN
 			workspaces
 		ON
-			forms.workspace_id = workspaces.id
+			forms.workspace_id = workspaces.ID
+		LEFT OUTER JOIN
+			form_elements
+		ON
+			form_elements.form_id = forms.ID
 		WHERE
 			forms.ID = $1
 		AND
 			workspaces.user_id = $2
 		`, formID, userID)
-	var form Form
-	var workspace Workspace
-	if err := row.Scan(&form.ID, &form.Title, &form.CreatedAt, &form.UpdatedAt,
-		&workspace.ID, &workspace.Name, &workspace.UserID, &workspace.CreatedAt,
-		&workspace.UpdatedAt); err != nil {
+	if err != nil {
 		log.Println(err)
-		if err == pgx.ErrNoRows {
-			w.WriteHeader(http.StatusForbidden)
-			return
-		}
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	if err = json.NewEncoder(w).Encode(map[string]interface{}{
-		"form":      form,
-		"workspace": workspace,
-	}); err != nil {
+
+	var rowsData []RowFormData
+	for rows.Next() {
+		var rowData RowFormData
+		if err := rows.Scan(
+			// forms
+			&rowData.FormID,
+			&rowData.FormTitle,
+			&rowData.FormCreatedAt,
+			&rowData.FormUpdatedAt,
+			// workspace
+			&rowData.WorkspaceID,
+			&rowData.WorkspaceName,
+			&rowData.WorkspaceCreatedAt,
+			&rowData.WorkspaceUpdatedAt,
+			// user
+			&rowData.UserID,
+			// form elements
+			&rowData.FormElementID,
+			&rowData.FormElementType,
+			&rowData.FormElementValue,
+		); err != nil {
+			log.Println(err)
+			continue
+		}
+		rowsData = append(rowsData, rowData)
+	}
+	formData, err := parseFormData(rowsData)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err = json.NewEncoder(w).Encode(formData); err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
