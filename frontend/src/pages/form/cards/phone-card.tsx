85affa6b59559c8badfa9024fe056a75ef92c1a4
anupam <anupamminz300am@gmail.com>
2025-05-08 15:52:16 +0530

    fix phone card

--- frontend/src/pages/form/cards/phone-card.tsx
+++ frontend/src/pages/form/cards/phone-card.tsx
@@ -1,10 +1,14 @@
+import { EditableParagraph } from "@/components/editable-input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Trash2 } from "lucide-react";
+import React from "react";
 import type { FormElement } from "../index";
 
 export const PhoneCard = ({ form }: { form: FormElement }) => {
+  const titleRef = React.useRef<HTMLParagraphElement>(null);
+  const descriptionRef = React.useRef<HTMLParagraphElement>(null);
 
   return (
     <>
@@ -26,10 +30,23 @@ export const PhoneCard = ({ form }: { form: FormElement }) => {
         </Button>
       </div>
 
+      <section className="grid gap-0">
+        <EditableParagraph
+          className="text-xl"
+          paragraphRef={titleRef}
+          data-placeholder="Your question here."
+          handleChange={()=>{}}
+        />
+        <EditableParagraph
+          className="text-base font-light"
+          paragraphRef={descriptionRef}
+          data-placeholder="Description (optional)"
+        />
+      </section>
       <input
-        placeholder="08173096350"
+        disabled
+        placeholder="9876543210"
         className="text-2xl placeholder:text-2xl focus:outline-0 border-b focus:border-b-2 border-blue-600"
-        value={form.value}
       />
     </>
   );
