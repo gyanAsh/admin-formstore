ea2e34016986717e4f3e1eb9c54e28784a1ae50b
gyanEkka <gyan.ekka@mathongo.com>
2025-05-29 01:50:48 +0530

    feat (form-preview): not elements in a form - case handled

--- frontend/src/pages/form/preview.tsx
+++ frontend/src/pages/form/preview.tsx
@@ -1,9 +1,11 @@
 import { $all_forms } from "@/store/forms/form-elements";
 import { useStore } from "@nanostores/react";
 import { useCallback, useMemo, useState } from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 
 import { AnimatePresence, motion } from "motion/react";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
 const variants = {
   enter: (direction: "up" | "down") => ({
     y: direction === "up" ? -100 : 100,
@@ -12,12 +14,12 @@ const variants = {
   center: {
     y: 0,
     opacity: 1,
-    transition: { duration: 0.5 },
+    transition: { duration: 0.3 },
   },
   exit: (direction: "up" | "down") => ({
     y: direction === "up" ? 100 : -100,
     opacity: 0,
-    transition: { duration: 0.5 },
+    transition: { duration: 0.3 },
   }),
 };
 
@@ -56,58 +58,78 @@ const FullPageScroll = () => {
   }, [currentSection, elements.length]);
 
   const currentElement = elements[currentSection];
+  if (elements.length > 0)
+    return (
+      <div className="relative bg-red-800 overflow-hidden h-[100dvh]">
+        {/* Progress Bar */}
+        <div
+          id="scroll-progress"
+          className="fixed rounded-r-[4px] top-0 left-0 z-50 h-2.5 bg-blue-500 transition-all ease-linear duration-600"
+          style={{
+            width: `${progressPercentage}%`,
+          }}
+        />
 
-  return (
-    <div className="relative bg-red-800 overflow-hidden h-[100dvh]">
-      {/* Progress Bar */}
-      <div
-        id="scroll-progress"
-        className="fixed rounded-r-[4px] top-0 left-0 z-50 h-2.5 bg-blue-500 transition-all duration-500"
-        style={{
-          width: `${progressPercentage}%`,
-        }}
-      />
+        {/* Sections */}
+        <div className="h-full overflow-hidden">
+          <AnimatePresence custom={direction} mode="wait">
+            {currentElement && (
+              <motion.div
+                key={currentElement.id}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                className="absolute w-full h-full"
+              >
+                <div className="h-full rounded-none bg-amber-500 flex items-center justify-center">
+                  {currentElement.field}
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
 
-      {/* Sections */}
-      <div className="h-full overflow-hidden">
-        <AnimatePresence custom={direction} mode="wait">
-          {currentElement && (
-            <motion.div
-              key={currentElement.id}
-              custom={direction}
-              variants={variants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              className="absolute w-full h-full"
-            >
-              <div className="h-full rounded-none bg-amber-500 flex items-center justify-center">
-                {currentElement.field}
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
+        {/* Navigation Buttons */}
+        <div className="fixed bottom-4 left-4 space-x-2">
+          <button
+            onClick={() => paginate("up")}
+            disabled={currentSection === 0}
+            className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => paginate("down")}
+            disabled={currentSection === elements.length - 1}
+            className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
+          >
+            Next
+          </button>
+        </div>
       </div>
+    );
+  else
+    return (
+      <div className="h-[100dvh] w-[100dvw] flex flex-col gap-10 items-center justify-center">
+        <h2 className="text-7xl text-red-500 font-bold">
+          Elements Not Found - 404{" "}
+        </h2>
 
-      {/* Navigation Buttons */}
-      <div className="fixed bottom-4 left-4 space-x-2">
-        <button
-          onClick={() => paginate("up")}
-          disabled={currentSection === 0}
-          className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
+        <Button
+          variant={"black"}
+          className="w-50 h-15 text-2xl group"
+          effect={"scale"}
+          asChild
         >
-          Previous
-        </button>
-        <button
-          onClick={() => paginate("down")}
-          disabled={currentSection === elements.length - 1}
-          className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
-        >
-          Next
-        </button>
+          <Link to={`/workspace/${workspaceId}/${formId}/create`}>
+            <ArrowLeft className="size-7 group-hover:-translate-x-5 duration-250 ease-linear" />
+            Go back{" "}
+          </Link>
+        </Button>
       </div>
-    </div>
-  );
+    );
 };
 
 export default FullPageScroll;
