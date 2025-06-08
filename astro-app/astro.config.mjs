ea7edcf0f510c215247b867374bc5382442ddfe0
gyanAsh <gyanashekka1738@gmail.com>
2025-06-08 15:07:44 +0530

    astro cloudflare config update

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -1,17 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   vite: {
-      plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: import.meta.env.PROD
+        ? { "react-dom/server": "react-dom/server.edge" }
+        : undefined,
     },
+  },
 
   integrations: [react()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
