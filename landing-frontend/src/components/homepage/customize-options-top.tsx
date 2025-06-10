0ac369ef2c7b932e2744c0ea5ff9b172319b6076
gyanAsh <gyanashekka1738@gmail.com>
2025-06-10 23:37:51 +0530

    feat (homepage): style options added

--- landing-frontend/src/components/homepage/customize-options-top.tsx
+++ landing-frontend/src/components/homepage/customize-options-top.tsx
@@ -1,13 +1,12 @@
-import { AnimatePresence } from "motion/react";
-import * as motion from "motion/react-client";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
+import { Sketch } from "@uiw/react-color";
 
 import { Button } from "../ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 import * as React from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Bold, Check, ChevronsUpDown, Italic } from "lucide-react";
 import {
   Command,
   CommandEmpty,
@@ -18,7 +17,11 @@ import {
 } from "@/components/ui/command";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-import { Separator } from "../ui/separator";
+import type {
+  PopoverProps,
+  PopoverTriggerProps,
+} from "@radix-ui/react-popover";
+import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
 
 const tabs = [
   { id: 1, title: "Label", code: "tab-1" },
@@ -34,7 +37,7 @@ const CustomizeOptionTop = () => {
   // });
   return (
     <>
-      <section className="border border-zinc-300 bg-zinc-100 w-full max-w-[750px] rounded-2xl overflow-hidden">
+      <section className="border border-zinc-300 bg-zinc-100 w-full max-w-[80dvw] lg:max-w-[750px] rounded-4xl overflow-hidden">
         {/* <nav>
           <ul className="flex gap-1 border-b border-zinc-950">
             {tabs.map((item, idx) => (
@@ -105,49 +108,8 @@ const CustomizeOptionTop = () => {
               Label
             </Button>
           </PopoverTrigger>
-          <PopoverContent align="start" className="w-80">
-            <div className="grid gap-4">
-              <div className="space-y-2">
-                <h4 className="leading-none font-medium">Dimensions</h4>
-                <p className="text-muted-foreground text-sm">
-                  Set the dimensions for the layer.
-                </p>
-              </div>
-              <div className="grid gap-2">
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="width">Width</Label>
-                  <Input
-                    id="width"
-                    defaultValue="100%"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="maxWidth">Max. width</Label>
-                  <Input
-                    id="maxWidth"
-                    defaultValue="300px"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="height">Height</Label>
-                  <Input
-                    id="height"
-                    defaultValue="25px"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="maxHeight">Max. height</Label>
-                  <Input
-                    id="maxHeight"
-                    defaultValue="none"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-              </div>
-            </div>
+          <PopoverContent align="start" className="w-85 rounded-4xl p-6">
+            <LabelDesignContent />
           </PopoverContent>
         </Popover>
         <Popover>
@@ -160,49 +122,8 @@ const CustomizeOptionTop = () => {
               Description
             </Button>
           </PopoverTrigger>
-          <PopoverContent className="w-80">
-            <div className="grid gap-4">
-              <div className="space-y-2">
-                <h4 className="leading-none font-medium">Dimensions</h4>
-                <p className="text-muted-foreground text-sm">
-                  Set the dimensions for the layer.
-                </p>
-              </div>
-              <div className="grid gap-2">
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="width">Width</Label>
-                  <Input
-                    id="width"
-                    defaultValue="100%"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="maxWidth">Max. width</Label>
-                  <Input
-                    id="maxWidth"
-                    defaultValue="300px"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="height">Height</Label>
-                  <Input
-                    id="height"
-                    defaultValue="25px"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-                <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="maxHeight">Max. height</Label>
-                  <Input
-                    id="maxHeight"
-                    defaultValue="none"
-                    className="col-span-2 h-8"
-                  />
-                </div>
-              </div>
-            </div>
+          <PopoverContent className="w-85 rounded-4xl p-6">
+            <DescriptionDesignContent />
           </PopoverContent>
         </Popover>
         <Popover>
@@ -215,7 +136,7 @@ const CustomizeOptionTop = () => {
               Buttons
             </Button>
           </PopoverTrigger>
-          <PopoverContent className="w-80">
+          <PopoverContent className="w-85 rounded-4xl p-6">
             <div className="grid gap-4">
               <div className="space-y-2">
                 <h4 className="leading-none font-medium">Dimensions</h4>
@@ -270,7 +191,7 @@ const CustomizeOptionTop = () => {
               Background
             </Button>
           </PopoverTrigger>
-          <PopoverContent align="end" className="w-80">
+          <PopoverContent align="end" className="w-85 rounded-4xl p-6">
             <div className="grid gap-4">
               <div className="space-y-2">
                 <h4 className="leading-none font-medium">Dimensions</h4>
@@ -322,73 +243,345 @@ const CustomizeOptionTop = () => {
 
 export default CustomizeOptionTop;
 
-const DesignContent = () => {
-  return <div>adfasdfasdfasdfsadfadsfasdf design</div>;
+const DescriptionDesignContent = () => {
+  const [labelSize, setLabelSize] = useState("60px");
+  const [labelFamily, setLabelFamily] = useState(fonts.at(0)?.value);
+  const [labelColor, setLabelColor] = useState("#fff");
+  const [labelStyle, setLabelStyle] = useState({
+    italic: false,
+    weight: "light",
+  });
+  return (
+    <div className="grid gap-4">
+      <div className="space-y-2">
+        <h4 className="leading-none font-medium">Description</h4>
+        <p className="text-muted-foreground text-sm">
+          Set the styles for the description.
+        </p>
+      </div>
+      <div className="grid gap-2 md:gap-4 lg:gap-5">
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="fontFamily">Font family</Label>
+
+          <ComboboxDemo
+            value={labelFamily}
+            setValue={setLabelFamily}
+            id="fontFamily"
+          />
+        </div>
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="textSize">Text Size</Label>
+          <div
+            className=" col-span-2 flex items-center justify-between gap-1"
+            id="textSize"
+          >
+            {textSize.map((size) => {
+              return (
+                <Button
+                  key={size.value}
+                  aria-selected={labelSize === size.value}
+                  className=" aria-[selected=true]:bg-zinc-900 aria-[selected=true]:text-zinc-50"
+                  variant={"outline"}
+                  size={"icon"}
+                  onClick={() => setLabelSize(size.value)}
+                >
+                  {size.name}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="textColor">Text Color</Label>
+          <ColorPicker hex={labelColor} setHex={setLabelColor} id="textColor" />
+        </div>
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="height">Styles</Label>
+          <div className=" col-span-2 flex items-center gap-1" id="textSize">
+            <Button
+              aria-selected={labelStyle.italic}
+              className="aria-[selected=true]:bg-zinc-900 aria-[selected=true]:text-zinc-50"
+              variant={"outline"}
+              size={"icon"}
+              onClick={() =>
+                setLabelStyle((e) => ({ ...e, italic: !e.italic }))
+              }
+            >
+              <Italic />
+            </Button>
+            <ToggleGroup
+              variant="outline"
+              value={labelStyle.weight}
+              onValueChange={(e) =>
+                setLabelStyle((el) => ({ ...el, weight: e }))
+              }
+              className="inline-flex"
+              type="single"
+            >
+              <ToggleGroupItem
+                className="data-[state=on]:bg-zinc-900 data-[state=on]:text-white"
+                value="light"
+              >
+                Aa
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="data-[state=on]:bg-zinc-900 data-[state=on]:text-white"
+                value="normal"
+              >
+                <Bold />
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="data-[state=on]:bg-zinc-900 data-[state=on]:text-white"
+                value="bold"
+              >
+                Aa
+              </ToggleGroupItem>
+            </ToggleGroup>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-const frameworks = [
+const textSize = [
   {
-    value: "next.js",
-    label: "Next.js",
+    value: "16px",
+    name: "sm",
   },
   {
-    value: "sveltekit",
-    label: "SvelteKit",
+    value: "20px",
+    name: "md",
   },
   {
-    value: "nuxt.js",
-    label: "Nuxt.js",
+    value: "30px",
+    name: "lg",
   },
   {
-    value: "remix",
-    label: "Remix",
+    value: "48px",
+    name: "xl",
   },
   {
-    value: "astro",
-    label: "Astro",
+    value: "60px",
+    name: "xxl",
   },
 ];
 
-function ComboboxDemo() {
+const LabelDesignContent = () => {
+  const [labelSize, setLabelSize] = useState("60px");
+  const [labelFamily, setLabelFamily] = useState(fonts.at(0)?.value);
+  const [labelColor, setLabelColor] = useState("#fff");
+  const [labelStyle, setLabelStyle] = useState({
+    italic: false,
+    weight: "light",
+  });
+  return (
+    <div className="grid gap-4">
+      <div className="space-y-2">
+        <h4 className="leading-none font-medium">Label</h4>
+        <p className="text-muted-foreground text-sm">
+          Set the styles for the label.
+        </p>
+      </div>
+      <div className="grid gap-2 md:gap-4 lg:gap-5">
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="fontFamily">Font family</Label>
+
+          <ComboboxDemo
+            value={labelFamily}
+            setValue={setLabelFamily}
+            id="fontFamily"
+          />
+        </div>
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="textSize">Text Size</Label>
+          <div
+            className=" col-span-2 flex items-center justify-between gap-1"
+            id="textSize"
+          >
+            {textSize.map((size) => {
+              return (
+                <Button
+                  key={size.value}
+                  aria-selected={labelSize === size.value}
+                  className=" aria-[selected=true]:bg-zinc-900 aria-[selected=true]:text-zinc-50"
+                  variant={"outline"}
+                  size={"icon"}
+                  onClick={() => setLabelSize(size.value)}
+                >
+                  {size.name}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="textColor">Text Color</Label>
+          <ColorPicker hex={labelColor} setHex={setLabelColor} id="textColor" />
+        </div>
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="height">Styles</Label>
+          <div className=" col-span-2 flex items-center gap-1" id="textSize">
+            <Button
+              aria-selected={labelStyle.italic}
+              className="aria-[selected=true]:bg-zinc-900 aria-[selected=true]:text-zinc-50"
+              variant={"outline"}
+              size={"icon"}
+              onClick={() =>
+                setLabelStyle((e) => ({ ...e, italic: !e.italic }))
+              }
+            >
+              <Italic />
+            </Button>
+            <ToggleGroup
+              variant="outline"
+              value={labelStyle.weight}
+              onValueChange={(e) =>
+                setLabelStyle((el) => ({ ...el, weight: e }))
+              }
+              className="inline-flex"
+              type="single"
+            >
+              <ToggleGroupItem
+                className="data-[state=on]:bg-zinc-900 data-[state=on]:text-white"
+                value="light"
+              >
+                Aa
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="data-[state=on]:bg-zinc-900 data-[state=on]:text-white"
+                value="normal"
+              >
+                <Bold />
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="data-[state=on]:bg-zinc-900 data-[state=on]:text-white"
+                value="bold"
+              >
+                Aa
+              </ToggleGroupItem>
+            </ToggleGroup>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const fonts = [
+  {
+    value: '"Cal Sans", sans-serif',
+    label: "Cal Sans",
+  },
+  {
+    value: '"IBM Plex Serif", serif',
+    label: "IBM Plex Serif",
+  },
+  {
+    value: '"Roboto", sans-serif',
+    label: "Roboto",
+  },
+  {
+    value: '"Playfair Display", serif',
+    label: "Playfair Display",
+  },
+  {
+    value: '"Lora", serif',
+    label: "Lora",
+  },
+
+  {
+    value: '"IBM Plex Sans", sans-serif',
+    label: "IBM Plex Sans",
+  },
+];
+
+function ColorPicker({
+  hex,
+  setHex,
+  ...props
+}: {
+  hex: string;
+  setHex: React.Dispatch<React.SetStateAction<string>>;
+} & React.ComponentProps<
+  React.ForwardRefExoticComponent<PopoverTriggerProps>
+>) {
+  const st = {
+    "--bg-color": hex,
+  } as React.CSSProperties;
+  return (
+    <Popover modal>
+      <PopoverTrigger asChild {...props}>
+        <Button
+          effect={"none"}
+          variant="outline"
+          role="combobox"
+          className="col-span-2 justify-between"
+        >
+          {hex || "Select Font..."}
+          <div style={st} className="size-7 rounded-md bg-[var(--bg-color)]" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent align="end" className="p-0 w-fit">
+        <Sketch
+          color={hex}
+          onChange={(color) => {
+            setHex(color.hex);
+          }}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+function ComboboxDemo({
+  value,
+  setValue,
+  ...props
+}: React.ComponentProps<
+  React.ForwardRefExoticComponent<PopoverTriggerProps>
+> & {
+  value: string | undefined;
+  setValue: React.Dispatch<React.SetStateAction<string | undefined>>;
+}) {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState(frameworks.at(0)?.value);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
+      <PopoverTrigger asChild {...props}>
         <Button
           effect={"none"}
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="col-span-2 justify-between"
         >
           {value
-            ? frameworks.find((framework) => framework.value === value)?.label
+            ? fonts.find((font) => font.value === value)?.label
             : "Select Font..."}
           <ChevronsUpDown className="opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
         <Command>
-          <CommandInput placeholder="Search framework..." className="h-9" />
+          <CommandInput placeholder="Search font..." className="h-9" />
           <CommandList>
-            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandEmpty>No font found.</CommandEmpty>
             <CommandGroup defaultValue={value}>
-              {frameworks.map((framework) => (
+              {fonts.map((font) => (
                 <CommandItem
-                  key={framework.value}
-                  value={framework.value}
+                  key={font.value}
+                  value={font.value}
                   onSelect={(currentValue) => {
                     setValue(currentValue === value ? "" : currentValue);
                     setOpen(false);
                   }}
                 >
-                  {framework.label}
+                  {font.label}
                   <Check
                     className={cn(
                       "ml-auto",
-                      value === framework.value ? "opacity-100" : "opacity-0"
+                      value === font.value ? "opacity-100" : "opacity-0"
                     )}
                   />
                 </CommandItem>
