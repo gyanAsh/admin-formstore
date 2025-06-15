a449fab018038c3dee0087bce162c581daa16223
gyanAsh <gyanashekka1738@gmail.com>
2025-06-15 20:46:30 +0530

    fix (editor):edit description added

--- frontend/src/components/forms/v1/details.tsx
+++ frontend/src/components/forms/v1/details.tsx
@@ -1,7 +1,15 @@
 import { cn } from "@/lib/utils";
 import { ThemeValues } from "@/store/forms/designs/values";
-import { $current_form, $get_design_label } from "@/store/forms/form-elements";
-import { maxMdTextSize, maxSmTextSize } from "@/store/forms/formV1Design";
+import {
+  $current_form,
+  $get_design_description,
+  $get_design_label,
+} from "@/store/forms/form-elements";
+import {
+  maxMdTextSize,
+  maxSmTextSize,
+  textSizeLineHeight,
+} from "@/store/forms/formV1Design";
 import { useStore } from "@nanostores/react";
 
 export const FormLabel = ({
@@ -38,27 +46,29 @@ export const FormDescription = ({
   className,
   ...props
 }: React.ComponentProps<"div">) => {
-  const currentForm = useStore($current_form);
-  const theme = currentForm.design.theme;
+  const design = useStore($get_design_description);
+  const style: Record<string, string> & React.CSSProperties = {
+    "--size": design.size,
+    "--md-size": maxMdTextSize[design.size],
+    "--sm-size": maxSmTextSize[design.size],
+    "--line-height": textSizeLineHeight[design.size],
+    "--family": design.family,
+    "--weight": design.weight,
+    "--text-color": design.color,
+    "--italics": design.italics ? "italic" : "normal",
+    "--letter-space": design.letter_spacing,
+  };
+
   return (
     <div
       className={cn(
-        " text-center whitespace-pre-line text-lg @[64rem]:text-2xl",
-        {
-          "text-zinc-300 font-['Roboto','sans-serif'] text-lg @[64rem]:text-xl":
-            theme == ThemeValues.luxe_minimal_noir.value,
-        },
-        {
-          "text-emerald-50 font-['Roboto','sans-serif'] font-light":
-            theme == ThemeValues.gradient_forest.value,
-        },
-        {
-          "font-['Playfair_Display','serif'] font-light italic":
-            theme == ThemeValues.luxe_minimal_forest.value,
-        },
+        "whitespace-pre-line ",
+        " text-[calc(var(--sm-size))] md:text-[calc(var(--md-size))] lg:text-[calc(var(--size))] [color:var(--text-color)]",
+        "[line-height:var(--line-height)] [font-style:var(--italics)] [font-family:var(--family)] font-[var(--weight)] tracking-[var(--letter-space)]",
 
         className
       )}
+      style={style}
       {...props}
     />
   );
