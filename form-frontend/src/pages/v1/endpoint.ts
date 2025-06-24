1277e07295e048e6d3ca25f529a07ed0373d7434
anupam <anupamminz300am@gmail.com>
2025-06-24 11:31:38 +0530

    secretly removing axios

--- form-frontend/src/pages/v1/endpoint.ts
+++ form-frontend/src/pages/v1/endpoint.ts
@@ -1,6 +1,4 @@
-import axios from "axios";
-
 export async function fetchForm(formId: string): Promise<any> {
-  const response = await axios.get<any>(`/api/published/form/${formId}`);
-  return response.data;
+  const res = await fetch(`/api/published/form/${formId}`);
+  return res.json();
 }
