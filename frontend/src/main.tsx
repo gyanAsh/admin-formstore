d6d18d619bfd477069b1b47df28a42468ee25634
gyanAsh <gyanashekka1738@gmail.com>
2025-07-19 14:45:46 +0530

    remove : boska font removed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import "./assets/fonts/Zodiak_Complete/Fonts/WEB/css/zodiak.css"; //zodiak fonts
-import "./assets/fonts/Boska_Complete/Fonts/WEB/css/boska.css"; //boska fonts
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
