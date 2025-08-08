45d7eb1ce8353a4f94f2a129231b489ca8cb2c56
anupam <anupamminz300am@gmail.com>
2025-08-08 12:24:45 +0530

    failing test for published form

--- backend-go/test/endpoints/publish_test.go
+++ backend-go/test/endpoints/publish_test.go
@@ -57,4 +57,27 @@ func TestPublishForm(t *testing.T) {
 	if resp.StatusCode != 200 {
 		t.Fatal(fmt.Errorf("resp: %v %v", resp.Status, resp.Body))
 	}
+	var data map[string]any
+	if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		t.Fatal(fmt.Errorf("json decode: publish body: %v", err))
+	}
+	publicID_s, ok := data["public_id"].(string)
+	if !ok {
+		t.Fatal(fmt.Errorf("public_id not found in response body"))
+	}
+	r = httptest.NewRequest("GET", "http://localhost:4000/api/published/form/{public_id}", nil)
+	r.SetPathValue("public_id", publicID_s)
+	r.Header.Add("Authorization", fmt.Sprintf("Bearer %s", AUTH_TOKEN))
+	w = httptest.NewRecorder()
+
+	s.PublishedFormDataHandler(w, r)
+
+	resp = w.Result()
+
+	if resp.StatusCode != 200 {
+		t.Fatal(fmt.Errorf("resp: %v", resp.Status))
+	}
+	if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		t.Fatal(fmt.Errorf("json decode: get published form %v", err))
+	}
 }
