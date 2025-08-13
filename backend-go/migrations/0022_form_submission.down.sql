4451ca1045e869f6ad79b9bb80813c18388936a6
anupam <anupamminz300am@gmail.com>
2025-08-13 19:45:29 +0530

    form submission migration

---
+++ backend-go/migrations/0022_form_submission.down.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+ALTER TABLE form_elements DROP CONSTRAINT form_elements_pkey CASCADE;
+ALTER TABLE form_elements DROP CONSTRAINT unique_form_id_seq_number;
+ALTER TABLE form_elements DROP COLUMN ID;
+
+ALTER TABLE form_elements ADD CONSTRAINT form_elements_pkey PRIMARY KEY (form_id, seq_number);
+
+DROP TABLE form_submissions;
+DROP TABLE submission_entries;
