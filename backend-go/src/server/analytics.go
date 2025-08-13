6faa34ad9baf22235c011cf20bcd873df167fb88
anupam <anupamminz300am@gmail.com>
2025-08-13 20:36:30 +0530

    improve form submission query

--- backend-go/src/server/analytics.go
+++ backend-go/src/server/analytics.go
@@ -8,12 +8,25 @@ import (
 )
 
 func (s *Service) FormAnalyticsDataHandler(w http.ResponseWriter, r *http.Request) {
+	userID, err := authenticate(r)
+	if err != nil {
+		log.Println(fmt.Errorf("form analytics auth error: %v", err))
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 	formID, err := strconv.Atoi(r.PathValue("form_id"))
 	if err != nil {
 		log.Println(fmt.Errorf("form analytics invalid form_id %v error: %v",
 			r.PathValue("form_id"),
 			err,
 		))
+		w.WriteHeader(http.StatusBadRequest)
+		if err = json.NewEncoder(w).Encode(map[string]any{
+			"message": "invalid value for form_id",
+		}); err != nil {
+			log.Println(fmt.Errorf("failed to write repsonse: %v", err))
+		}
+		return
 	}
 	rows, err := s.Conn.Query(r.Context(), `
 		SELECT submission_entries.ID, data FROM submission_entries
@@ -22,6 +35,8 @@ func (s *Service) FormAnalyticsDataHandler(w http.ResponseWriter, r *http.Reques
 	`, formID)
 	if err != nil {
 		log.Println(fmt.Errorf("form analytics database query: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	var submissions []map[string]any
 	for rows.Next() {
@@ -38,5 +53,6 @@ func (s *Service) FormAnalyticsDataHandler(w http.ResponseWriter, r *http.Reques
 	}); err != nil {
 		log.Println(fmt.Errorf("form analytics failed to write json: %v", err))
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
