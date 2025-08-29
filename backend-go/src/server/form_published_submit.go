e09efd16d6b44bac3c99cbfb79875ef677af360d
anupam <anupamminz300am@gmail.com>
2025-08-29 13:42:28 +0530

    create submit endpoint

--- backend-go/src/server/form_published_submit.go
+++ backend-go/src/server/form_published_submit.go
@@ -20,11 +20,22 @@ func (s *Service) PublishedFormSubmitHandler(w http.ResponseWriter, r *http.Requ
 		}
 		return
 	}
-	if err = json.NewEncoder(w).Encode(map[string]any{
+	publicID := "placeholderpublicid"
+	if _, err := s.Conn.Exec(r.Context(), `
+		INSERT INTO form_submissions (form_id)
+		SELECT ID FROM forms
+		WHERE public_id = $1 AND status = 'published'`, publicID); err != nil {
+		log.Println(fmt.Errorf("submit form db insert query: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(map[string]any{
 		"message": "form successfully submitted",
 	}); err != nil {
-		log.Println(fmt.Errorf("json encoding success message failed: %v"err))
+		log.Println(fmt.Errorf("json encoding success message failed: %v", err))
 		return
 	}
 	log.Println(rawData)
 }
+
+
