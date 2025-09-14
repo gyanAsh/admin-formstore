916fe81f67aa0c48b1a18c44c25d72a424960647
anupam <anupamminz300am@gmail.com>
2025-09-14 15:09:32 +0530

    display form submission in a crude format

--- frontend/src/pages/form/analytics/submissions-analysis.tsx
+++ frontend/src/pages/form/analytics/submissions-analysis.tsx
@@ -4,62 +4,69 @@ import { FormElementIcon } from "@/store/forms/values";
 import { ClockArrowUp } from "lucide-react";
 import React from "react";
 
-export const SubmissionsAnalysis = ({submissions}: {submissions: any}) => {
-  return (
-    <section className="grid gap-2.5">
-      <div className=" grid grid-cols-5 gap-2.5 rounded-xl text-wrap w-full min-h-fit text-sm pt-1.5 pl-3 pr-6">
-        <div className="flex items-center gap-0.5 min-w-0">
-          <h2 className="text-zinc-500 dark:text-zinc-400">Submission No.</h2>
-        </div>
-        <div className="flex items-center gap-0.5 min-w-0">
-          <div className="p-1 rounded-[7px] group-hover:border bg-gray-200/95 dark:bg-gray-500/45">
-            <ClockArrowUp className="size-4" />
-          </div>
-          <h2 className="text-zinc-500 dark:text-zinc-400">Submitted At</h2>
-        </div>
-        <div className="flex items-center gap-1.5 min-w-0">
-          <TitleWithIcon value={FormFields.text}>Text</TitleWithIcon>
-        </div>
-        <div className="flex items-center gap-1.5 min-w-0">
-          <TitleWithIcon value={FormFields.email}>Email</TitleWithIcon>
-        </div>
-        <div className="flex items-center justify-end gap-0.5 min-w-0">
-          <h2 className="text-zinc-500 dark:text-zinc-400">Full Submission</h2>
-        </div>
-      </div>
+function DisplaySubmissionElementType({ type }: { type: string }) {
+  if (type == "email") {
+    return <TitleWithIcon value={FormFields.email}>Email</TitleWithIcon>;
+  } else if (type == "text") {
+    return <TitleWithIcon value={FormFields.text}>Text</TitleWithIcon>;
+  } else {
+    return <TitleWithIcon value={FormFields.text}>Value</TitleWithIcon>;
+  }
+}
 
-      <div className=" grid grid-cols-5 transition-all duration-75 bg-slate-50 dark:bg-slate-900/80 border-[1.5px] border-muted-foreground/30 hover:border-primary gap-2.5 rounded-xl text-wrap w-full min-h-fit text-sm py-2 px-6">
-        <p className="flex items-center font-medium break-all">1</p>
-        <p className="flex items-center font-medium break-all">5th June,2025</p>
-        <p className="flex items-center font-medium break-all">
-          Gyan Ashish Ekka
-        </p>
-        <p className="flex items-center font-medium break-all">
-          gyanashekka1738@gmail.com
-        </p>
-        <div className="flex justify-end items-center gap-0.5 min-w-0">
-          <button className="rounded-lg bg-black dark:bg-white text-white dark:text-black p-2 cursor-pointer hover:scale-105 active:scale-95 transition-all ease-in-out duration-75 w-fit">
-            View Details
-          </button>
+export const SubmissionsAnalysis = ({ submissions }: { submissions: any }) => {
+  if (submissions && submissions.length > 0) {
+    return (
+      <section className="grid gap-2.5">
+        <div className="grid min-h-fit w-full grid-cols-5 gap-2.5 rounded-xl pt-1.5 pr-6 pl-3 text-sm text-wrap">
+          <div className="flex min-w-0 items-center gap-0.5">
+            <h2 className="text-zinc-500 dark:text-zinc-400">Submission No.</h2>
+          </div>
+          <div className="flex min-w-0 items-center gap-0.5">
+            <div className="rounded-[7px] bg-gray-200/95 p-1 group-hover:border dark:bg-gray-500/45">
+              <ClockArrowUp className="size-4" />
+            </div>
+            <h2 className="text-zinc-500 dark:text-zinc-400">Submitted At</h2>
+          </div>
+          <div className="flex min-w-0 items-center gap-1.5">Username</div>
+          {submissions[0].elements.map((elem) => (
+            <div className="flex min-w-0 items-center gap-1.5" key={elem.type}>
+              <DisplaySubmissionElementType type={elem.type} />
+            </div>
+          ))}
+          {/* <div className="flex min-w-0 items-center justify-end gap-0.5"> */}
+          {/*   <h2 className="text-zinc-500 dark:text-zinc-400"> */}
+          {/*     Full Submission */}
+          {/*   </h2> */}
+          {/* </div> */}
         </div>
-      </div>
-      <div className=" grid grid-cols-5 transition-all duration-75 bg-slate-50 dark:bg-slate-900/80 border-[1.5px] border-muted-foreground/30 hover:border-primary gap-2.5 rounded-xl text-wrap w-full min-h-fit text-sm py-2 px-6">
-        <p className="flex items-center font-medium break-all">2</p>
-        <p className="flex items-center font-medium break-all">
-          15th June,2025
-        </p>
-        <p className="flex items-center font-medium break-all">Anupam Minz</p>
-        <p className="flex items-center font-medium break-all">
-          aamiz@protonmail.com
-        </p>
-        <div className="flex justify-end items-center gap-0.5 min-w-0">
-          <button className="rounded-lg bg-black dark:bg-white text-white dark:text-black p-2 cursor-pointer hover:scale-105 active:scale-95 transition-all ease-in-out duration-75 w-fit">
-            View Details
-          </button>
-        </div>
-      </div>
-    </section>
-  );
+
+        {submissions.map((sub) => (
+          <div className="border-muted-foreground/30 hover:border-primary grid min-h-fit w-full grid-cols-5 gap-2.5 rounded-xl border-[1.5px] bg-slate-50 px-6 py-2 text-sm text-wrap transition-all duration-75 dark:bg-slate-900/80">
+            <p className="flex items-center font-medium break-all">1</p>
+            <p className="flex items-center font-medium break-all">
+              {sub?.createdAt ?? "Jun 5th 2025"}
+            </p>
+            <p className="flex items-center font-medium break-all">
+              {sub?.username ?? "N.A."}
+            </p>
+            {sub.elements.map((elem) => (
+              <p className="flex items-center font-medium break-all">
+                {elem.value}
+              </p>
+            ))}
+            <div className="flex min-w-0 items-center justify-end gap-0.5">
+              <button className="w-fit cursor-pointer rounded-lg bg-black p-2 text-white transition-all duration-75 ease-in-out hover:scale-105 active:scale-95 dark:bg-white dark:text-black">
+                View Details
+              </button>
+            </div>
+          </div>
+        ))}
+      </section>
+    );
+  } else {
+    return <div>no submission, yet</div>;
+  }
 };
 
 const TitleWithIcon = ({
@@ -74,7 +81,7 @@ const TitleWithIcon = ({
     <React.Fragment>
       <div
         className={cn(
-          "p-1 rounded-[7px] group-hover:border",
+          "rounded-[7px] p-1 group-hover:border",
           { "bg-gray-200/95 dark:bg-gray-500/45": true },
           {
             "bg-blue-200/95 dark:bg-blue-500/45": el.color === "blue",
